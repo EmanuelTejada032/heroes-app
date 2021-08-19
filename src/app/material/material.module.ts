7012bd5f188f3f078022ad07f9e82599a8479b59
EmanuelTejada032 <67421425+EmanuelTejada032@users.noreply.github.com>
2021-08-19 08:56:52 -0400

    Add sidevar elements

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -13,6 +14,7 @@ import {MatIconModule} from '@angular/material/icon';
   ],
   exports: [
     MatButtonModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule
