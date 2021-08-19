520a15e628e6e5e55a6bc1f293d22ff5ea2dc24f
EMANUEL <emanueltejada31295@gmail.com>
2021-08-18 22:07:13 -0700

    basic angular material setup, first sidenav setup

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { AddHeroComponent } from './pages/add-hero/add-hero.component';
 import { SearchHeroComponent } from './pages/search-hero/search-hero.component';
 import { SeeHeroComponent } from './pages/see-hero/see-hero.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+
+
 
 
 
@@ -19,7 +25,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    FlexLayoutModule,
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
