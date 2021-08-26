93e6695ab0a697bddad0829fb01a5ff263ce6ae5
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-08-26 11:23:56 -0400

    fakse auth basic setup

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authServices: AuthService) { }
 
   ngOnInit(): void {
+  }
+  
+  onLoginClick(){
+    this.authServices.login()
+        .subscribe( user => {
+           console.log(user);
+          (user.id)? this.router.navigate(["/heroes"]): null;
+        })
+    // this.router.navigate(["/heroes"])
   }
 
 }
