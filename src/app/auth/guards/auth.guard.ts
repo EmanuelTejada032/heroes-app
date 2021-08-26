5a98dbf3baaeaea6bc44b145a06c51a0b8fec3e7
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-08-26 12:28:59 -0400

    Angular canActive guard, fake auth logout and persistent data with localstorage

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,26 +1,52 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
-  constructor(private authService: AuthService){}
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      
+      
+      return this.authService.verifyAuthUser()
+                 .pipe(
+                   tap( isAuth => {
+                     if(!isAuth){
+                       this.router.navigate(["/auth/login"])
+                     }
+                   })
+                 );
+      // if(this.authService.auth.id){
+      //   return true;
+      // }
+      // console.log("Locked by authGuard - canActivate")
+      // return false;
+  }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean  {
-      if(this.authService.auth.id){
-        return true;
-      }
-      console.log("Locked by authGuard")
-      return false;
+
+      return this.authService.verifyAuthUser()
+      .pipe(
+        tap( isAuth => {
+          if(!isAuth){
+            this.router.navigate(["/auth/login"])
+          }
+        })
+      );
+      // if(this.authService.auth.id){
+      //   return true;
+      // }
+      // console.log("Locked by authGuard - canLoads")
+      // return false;
       
     
   }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
 import { environment } from 'src/environments/environment';
+
 import { Auth } from '../interfaces/auth.interfaces';
 
 @Injectable({
@@ -17,15 +21,31 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  verifyAuthUser(): Observable<boolean>{
+    if(!localStorage.getItem("token")){
+      return of(false)// of creates observable with value provided as argument
+    }
+
+    return this.http.get<Auth>(`${this.baseUrl}/users/1`)
+               .pipe(
+                 map( auth => {
+                   this._auth = auth;
+                   return true
+                 })
+               ) 
+  }
+
   login(){
     return this.http.get<Auth>(`${this.baseUrl}/users/1`)
                     .pipe(
-                      tap( auth => this._auth = auth)
+                      tap( auth => this._auth = auth),
+                      tap( auth => localStorage.setItem("token", auth.id))
                     )
   }
 
   logout(){
     this._auth = undefined;
+    localStorage.removeItem("token")
   }
 
 }
